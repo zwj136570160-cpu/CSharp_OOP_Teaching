a5985eb864a636dfaff543a5b7071c254d7511e8
ZWJ136570160-CPU <zwj136570160@gmail.com>
2026-01-10 10:46:43 +0800

    添加项目文件。

---
+++ lesson1_枚举/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lesson1_枚举
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
