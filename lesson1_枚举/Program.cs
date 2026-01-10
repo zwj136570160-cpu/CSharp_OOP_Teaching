62940cbd1aa94f4034d842d4ec6df4227ee4df13
ZWJ136570160-CPU <zwj136570160@gmail.com>
2026-01-10 10:46:43 +0800

    课时3：枚举

---
+++ lesson1_枚举/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace lesson1_枚举
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("枚举");
+
+            #region 知识点一 基本概念
+
+            #endregion
+
+            #region 知识点二 在哪申明枚举
+
+            #endregion
+
+            #region 知识点三 枚举的使用
+
+            #endregion
+
+            #region 知识点四 枚举的类型转换
+
+            #endregion
+
+            #region 知识点五 枚举的作用
+
+            #endregion
+        }
+    }
+}
